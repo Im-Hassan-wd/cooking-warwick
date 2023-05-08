58aeee43243f90da62bd09b7de8b75c0387ad4b0
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-08 23:36:29 +0100

    deleting doc

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Trash from "../assets/trash.svg";
+import { projectFirestore } from "../firebase/config";
+// hooks
+import { useTheme } from "../hooks/useTheme";
+
 // styles
 import "./RecipeList.css";
-import { useTheme } from "../hooks/useTheme";
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme();
@@ -9,6 +13,10 @@ export default function RecipeList({ recipes }) {
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
   }
+
+  const handleClick = (id) => {
+    projectFirestore.collection("recipes").doc(id).delete();
+  };
 
   return (
     <div className="recipe-list">
@@ -18,6 +26,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime}</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className="delete"
+            src={Trash}
+            alt="trash can"
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
