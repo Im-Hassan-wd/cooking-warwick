44d4da61674e73cfd80d8e2c35318b56071cb3af
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-08 07:04:50 +0100

    theme context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 // styles
 import "./Navbar.css";
@@ -7,8 +9,9 @@ import Searchbar from "./Seachbar";
 //components
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link className="brand" to="/">
           <h1>Coooking Warwick</h1>
