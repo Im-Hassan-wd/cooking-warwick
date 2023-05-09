cb453e12e5f25c11545964faa6df33ea30e38f14
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-09 14:41:21 +0100

    realtime collection

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -17,19 +17,25 @@ export default function Home() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore.collection("recipes").onSnapshot((snapshot) => {
-      if (snapshot.empty) {
-        setError("No recipes to load");
-        setIsPending(false);
-      } else {
-        let results = [];
-        snapshot.docs.forEach((doc) => {
-          results.push({ id: doc.id, ...doc.data() });
-        });
-        setData(results);
+    const unsub = projectFirestore.collection("recipes").onSnapshot(
+      (snapshot) => {
+        if (snapshot.empty) {
+          setError("No recipes to load");
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      },
+      (err) => {
+        setError(err.message);
         setIsPending(false);
       }
-    });
+    );
   }, []);
 
   return (
