9a1a9bd891e9b15b2c2a74514bcd25a94fa12e49
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-09 15:07:04 +0100

    finishing up

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -17,11 +17,10 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
+    const unsub = projectFirestore
       .collection("recipes")
       .doc(id)
-      .get()
-      .then((doc) => {
+      .onSnapshot((doc) => {
         if (doc.exists) {
           setIsPending(false);
           setRecipe(doc.data());
@@ -30,11 +29,13 @@ export default function Recipe() {
           setError("Could nt find that recipe");
         }
       });
+
+    return () => unsub();
   }, [id]);
 
   const handleClick = () => {
     projectFirestore.collection("recipes").doc(id).update({
-      title: "Something compeletly different",
+      title: "Veg pie",
     });
   };
 
